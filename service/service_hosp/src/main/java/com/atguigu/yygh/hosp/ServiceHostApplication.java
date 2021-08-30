501df9cead6ae3c2e196e39d786d8106029c603a
gaich <1432351805@qq.com>
2021-08-30 18:10:30 +0800

    add

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHostApplication.java
@@ -0,0 +1,11 @@
+package com.atguigu.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHostApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHostApplication.class,args);
+    }
+}
