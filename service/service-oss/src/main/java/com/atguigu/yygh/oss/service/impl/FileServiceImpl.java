dcb59f4230a02a98b080b3bc4ab2bdd6988c8cab
gaich <1432351805@qq.com>
2021-11-01 18:11:29 +0800

    到136

--- service/service-oss/src/main/java/com/atguigu/yygh/oss/service/impl/FileServiceImpl.java
+++ service/service-oss/src/main/java/com/atguigu/yygh/oss/service/impl/FileServiceImpl.java
@@ -16,8 +16,6 @@ public class FileServiceImpl implements FileService {
         // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
         String endpoint = "https://oss-cn-beijing.aliyuncs.com";
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5t7m6rbrKD6QbQjTMHFn";
-        String accessKeySecret = "LvhQloUnoEevcR7PW126cBvLZu8WBC";
 
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
